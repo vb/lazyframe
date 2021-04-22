2ee661734803dfb36655b39d7628d5e66bda84c2
zhouyi <zhouyi@stockpalm.com>
2021-04-22 12:03:51 +0800

    add flag switch if autoplay iframe video

--- src/lazyframe.js
+++ src/lazyframe.js
@@ -16,6 +16,7 @@ const Lazyframe = () => {
     y: undefined,
     debounce: 250,
     lazyload: true,
+    autoplay: false,
     initinview: false,
     onLoad: (l) => {},
     onAppend: (l) => {},
@@ -342,7 +343,9 @@ const Lazyframe = () => {
     iframeNode.setAttribute('src', settings.src);
     iframeNode.setAttribute('frameborder', 0);
     iframeNode.setAttribute('allowfullscreen', '');
-    iframeNode.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+    if (settings.autoplay) {
+      iframeNode.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+    }
 
     if (settings.vendor === 'vine') {
       const scriptNode = document.createElement('script');
